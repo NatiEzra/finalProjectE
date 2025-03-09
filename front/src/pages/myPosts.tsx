83fd82926c859b3cfb3684a8566d511c66f76d31
Ofek Segman <ofeksegman@gmail.com>
2025-03-09 12:27:11 +0200

    myPosts

---
+++ front/src/pages/myPosts.tsx
@@ -0,0 +1,37 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import "../css/myPosts.css";
+import FeedPage from "./feedPage";
+
+
+
+interface Post {
+    _id: string;
+    SenderId: string;
+    title: string;
+    content: string;
+    image?: string;
+    userLikes: Array<string>;
+    comments: number;
+    date: Date;
+  }
+  
+
+const MyPosts: React.FC = () => {
+      const [posts, setPosts] = useState<Post[]>([]);
+
+      const userId = localStorage.getItem("id") || "";
+      const token = localStorage.getItem("accessToken"); 
+      const  email = localStorage.getItem("email");
+      const  name = localStorage.getItem("name");
+      const  image = localStorage.getItem("image");
+    
+    return (
+        <FeedPage filter={userId}></FeedPage>)
+    
+}
+
+
+
+export default MyPosts;
+
+
